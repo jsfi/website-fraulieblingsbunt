84fac5621a93a730fce79378585121b30eac739f
Martin Sachse <martin.sachse+jsfi@gmail.com>
2015-12-05 14:01:05 +0100

    create own CSS instead of extending twentyfifteen

--- htdocs/wp-content/themes/fraulieblingsbunt/functions.php
+++ htdocs/wp-content/themes/fraulieblingsbunt/functions.php
@@ -1,3 +1,9 @@
 <?php
 
-add_filter('show_admin_bar', '__return_false');
+add_filter( 'show_admin_bar', '__return_false' );
+
+add_action( 'wp_enqueue_scripts', function() {
+    wp_dequeue_script( 'twentyfifteen-skip-link-focus-fix' );
+    wp_dequeue_script( 'twentyfifteen-keyboard-image-navigation' );
+    wp_dequeue_script( 'twentyfifteen-script' );
+}, 100 );
